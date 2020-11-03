b83a2a8e961363204a2e2de609c1f7e982f69958
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2020-11-03 18:18:29 -0300

    numero 02 ok

---
+++ Lista/JavaApplication2/src/javaapplication2/Principal.java
@@ -0,0 +1,54 @@
+package javaapplication2;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+class Count extends Thread {
+
+    Count() {
+        System.out.println("Iniciando as threads...");
+        start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 5; i++) {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                Date hora = Calendar.getInstance().getTime(); // Ou qualquer outra forma que tem
+                String dataFormatada = sdf.format(hora);
+                //instância um objeto da classe Random usando o construtor básico
+                Random gerador = new Random();
+
+                int k = gerador.nextInt(26);
+
+                System.out.println(dataFormatada);
+                Thread.sleep(5000);
+                System.out.println("Noticia " + k);
+                Thread.sleep(5000);
+                System.out.println("Noticia " + i);
+                if (i == 5) {
+                    System.exit(-1);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("A thread parou");
+        }
+
+    }
+}
+
+class ExtendingExample {
+
+    public static void main(String args[]) {
+        Count cnt = new Count();
+        try {
+            while (cnt.isAlive()) {
+                Thread.sleep(10);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("A thread parou");
+        }
+    }
+}
