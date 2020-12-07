63611d9d59379e36c519130895afd0471aa5f2dc
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2020-12-07 10:29:53 -0300

    exercicio 02

--- Lista 2/JavaApplication2/src/JavaApplication2/Consumer.java
+++ Lista 2/JavaApplication2/src/JavaApplication2/Consumer.java
@@ -1,4 +1,3 @@
-
 package JavaApplication2;
 
 // A classe Consumer representa a thread consumidora em um relacionamento
@@ -6,43 +5,45 @@ package JavaApplication2;
 // O método run da classe Consumer itera dez vezes lendo um valor do buffer
 import java.util.Random;
 
-public class Consumer implements Runnable
-{
+public class Consumer implements Runnable {
+
     private static Random generator = new Random();
     private Buffer sharedLocation; // referência a objeto compartilhado
-    
+
     // construtor
-    public Consumer( Buffer shared )
-    {
+    public Consumer(Buffer shared) {
         sharedLocation = shared;
     } // fim do construtor Producer
-    
+
     // lê o valor de sharedLocation quatro vezes e soma os valores
     @Override
-    public void run() 
-    {
+    public void run() {
         int sum = 0;
-        
-        for (int count = 1; count <= 10; count++)
-        {
-            try // dorme de 0 a 3 segundos, então lê valor no buffer
-            {
-                // a thread dorme... espera sincronizada
-                Thread.sleep(generator.nextInt(3000)); 
-                sum += sharedLocation.get(); // adiciona valor lido à soma
-                //System.out.printf("\t\t\t\t%2d\n", sum); // imprime o somatório
-            } // fim do try
-            // se a thread adormecida é interrompida, imprime rastreamento de pilha
-            catch (InterruptedException exception)
-            {
-                exception.printStackTrace();
-            } // fim do catch
-        } // fim do for
-        
-        System.out.printf("\n%s %d.\n%s\n", 
-                "A soma dos valores lidos pelo Consumidor foi ", 
-                sum,
-                "Fim do Consumidor!\n");
-    } // fim método run    
+
+        for (int count = 1; count <= 5; count++) {
+            Random gerador = new Random();
+            int k = gerador.nextInt(10);
+            
+            //numeros primos
+            if (k % count == 0) {
+                try // dorme de 0 a 3 segundos, então lê valor no buffer
+                {
+                    // a thread dorme... espera sincronizada
+                    Thread.sleep(1000);
+                    sum += sharedLocation.get(); // adiciona valor lido à soma
+                    //System.out.printf("\t\t\t\t%2d\n", sum); // imprime o somatório
+                } // fim do try
+                // se a thread adormecida é interrompida, imprime rastreamento de pilha
+                catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                } // fim do catch
+            } // fim do for
+
+            System.out.printf("\n%s %d.\n%s\n",
+                    "A soma dos valores primos lidos pelo Consumidor foi ",
+                    sum,
+                    "Fim do Consumidor!\n");
+        } // fim método run 
+    }
 } // fim da classe Consumer
 
--- Lista 2/JavaApplication2/src/JavaApplication2/Producer.java
+++ Lista 2/JavaApplication2/src/JavaApplication2/Producer.java
@@ -1,4 +1,3 @@
-
 package JavaApplication2;
 
 // A classe Producer representa a thread Produtora em um relacionamento
@@ -6,42 +5,45 @@ package JavaApplication2;
 import java.util.Random;
 
 // O método run de Producer armazena os valores de 1 a 10 no buffer
-public class Producer implements Runnable
-{
+public class Producer implements Runnable {
+
     private static Random generator = new Random(); // gerador de números aleatórios
     private Buffer sharedLocation;  // referência a objeto compartilhado
-    
+
     // construtor
-    public Producer( Buffer shared )
-    {
+    public Producer(Buffer shared) {
         sharedLocation = shared;
     } // fim do construtor Producer
 
     // armazena os valores de 1 a 10 em sharedLocation
     @Override
-    public void run() 
-    {
+    public void run() {
         int sum = 0;
-        
-        for (int count = 1; count <= 10; count++)
-        {
-            try // dorme de 0 a 3 segundos, então coloca valor no buffer
-            {
-                // a thread dorme... espera sincronizada
-                Thread.sleep(generator.nextInt(3000)); 
-                sharedLocation.set(count); // configura o valor no buffer
-                sum += count; // incrementa a soma dos valores
-                //System.out.printf("\t\t%2d\n", sum); // imprime o somatório
-            } // fim do try
-            // se a thread adormecida é interrompida, imprime rastreamento de pilha
-            catch (InterruptedException exception)
-            {
-                exception.printStackTrace();
-            } // fim do catch
-        } // fim do for
-        
-        System.out.printf("\n%s\n%s\n", 
-                "Produtor terminou a produção de dados", 
-                "Fim do Produtor!\n");
-    } // fim do método run
+
+        for (int count = 1; count <= 5; count++) {
+            Random gerador = new Random();
+            int k = gerador.nextInt(10);
+            
+            //numeros primos
+            if (k % count == 0) {
+                try // dorme de 0 a 3 segundos, então coloca valor no buffer
+                {
+
+                    // a thread dorme... espera sincronizada
+                    Thread.sleep(1000);
+                    sharedLocation.set(count); // configura o valor no buffer
+                    sum += count; // incrementa a soma dos valores
+                    //System.out.printf("\t\t%2d\n", sum); // imprime o somatório
+                } // fim do try
+                // se a thread adormecida é interrompida, imprime rastreamento de pilha
+                catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                } // fim do catch
+            } // fim do for
+
+            System.out.printf("\n%s\n%s\n",
+                    "Produtor terminou a produção de dados",
+                    "Fim do Produtor!\n");
+        } // fim do método run
+    }
 } // fim classe Producer
